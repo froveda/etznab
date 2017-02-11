efc0f6f7c91deace83d6a45a1f32cd8d0a8b38bc
Fabricio roveda <froveda@gmail.com>
2017-02-11 15:00:31 -0300

    Add location

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -32,3 +32,16 @@ $(document).ready(function(){
     }
   });
 });
+
+function initMap() {
+  var uluru = {lat: -34.589859, lng: -58.432122};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
