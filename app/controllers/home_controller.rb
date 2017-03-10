15fa4b6004a0a536be1405e65f131b2a7caf8474
Fabricio roveda <froveda@gmail.com>
2017-03-10 15:21:31 -0300

    Move collections get from view to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+    @main_header_images = MainHeaderImage.all
+    @instagram_images = InstagramImage.show_enabled
+    @services = Service.all
+    @promos = Promo.all
+  end
 end
